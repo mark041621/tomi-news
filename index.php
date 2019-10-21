8370e5d74461c91b1f63686d210f27c8b886ac01
maaku <mk.pst0529@gmail.com>
2019-10-22 01:23:48 +0900

    スクレイピングでニュースを取得

--- index.php
+++ index.php
@@ -1,3 +1,43 @@
 <?php
-    echo "Hello World";
-?>
+    require_once("./phpQuery-onefile.php");
+
+    list($dateArray, $newsArray) = getNews();
+
+    $tweet = "富田美憂さんの最新ニュース\r\n\r\n";
+    for ($i=0; $i<2; $i++) {
+        $tweet = $tweet . $dateArray[$i] . "\r\n";
+        $tweet = $tweet . $newsArray[$i] . "\r\n\r\n";
+    }
+    
+    echo $tweet;
+
+    function getNews() {
+        $html = file_get_contents("https://columbia.jp/tomitamiyu/index.html");
+        
+        $dateArray = array();
+        $newsArray = array();
+        
+        for ($i=0; $i<2; $i++) {
+            $dateArray[$i] = phpQuery::newDocument($html)
+            ->find("#wrapper")
+            ->find(".container")
+            ->find(".col2.clearfix")
+            ->find(".newsCol")
+            ->find(".articleWrap")
+            ->find("dl")
+            ->find("dt:eq(".$i.")")
+            ->text();
+    
+            $newsArray[$i] = phpQuery::newDocument($html)
+            ->find("#wrapper")
+            ->find(".container")
+            ->find(".col2.clearfix")
+            ->find(".newsCol")
+            ->find(".articleWrap")
+            ->find("dl")
+            ->find("dd:eq(".$i.")")
+            ->text();
+        }
+        return [$dateArray, $newsArray];
+    }
+?>  
